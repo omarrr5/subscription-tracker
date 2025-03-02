6364d5ff7813c3cad8bd02553332b8a1070221dc
omar tarik yehya <khalaf.omar20@gmail.com>
2025-03-02 11:23:17 +0200

    feat: Authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { signUp, signIn, signOut } from '../controllers/auth.controller.js';
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => res.send({ title: "Sign Up" }));
-authRouter.post("/sign-in", (req, res) => res.send({ title: "Sign In" }));
-authRouter.post("/sign-out", (req, res) => res.send({ title: "Sign Out" }));
+// Path: /api/v1/auth/sign-up (POST)
+authRouter.post("/sign-up", signUp);
+// Path: /api/v1/auth/sign-in (POST)
+authRouter.post("/sign-in", signIn);
+// Path: /api/v1/auth/sign-out (POST)
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
