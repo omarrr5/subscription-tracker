e01d1e5bcd1dd85593cf6e7b4db7e1e4a1e26742
omar tarik yehya <khalaf.omar20@gmail.com>
2025-03-02 11:46:33 +0200

    feat: authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => res.send({ title: "Get all users" }));
-userRouter.get("/:id", (req, res) => res.send({ title: "Get user details" }));
+userRouter.get("/", authorize, getUsers);
+userRouter.get("/:id", authorize, getUser);
 userRouter.post("/", (req, res) => res.send({ title: "Create new user" }));
 userRouter.put("/:id", (req, res) => res.send({ title: "Update User" }));
 userRouter.delete("/:id", (req, res) => res.send({ title: "Delete User" }));
